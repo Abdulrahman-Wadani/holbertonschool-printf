17b61176ae34505d1cc2d307b29824fab0816963
Sara <sarakhalid.b28@gmail.com>
2026-04-16 13:40:56 +0300

    Add functions to print characters and percent sign

--- print_chars.c
+++ print_chars.c
@@ -1,1 +1,24 @@
 /* sara to do */
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * print_char - prints a character
+ * @c: character to print
+ *
+ * Return: 1 (number of characters printed)
+ */
+int print_char(char c)
+{
+	return (write(1, &c, 1));
+}
+
+/**
+ * print_percent - prints %
+ *
+ * Return: 1
+ */
+int print_percent(void)
+{
+	return (write(1, "%", 1));
+}
